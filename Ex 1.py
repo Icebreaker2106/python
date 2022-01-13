a24b06c44f52d5363cbcec806795619cc5f8122e
Icebreaker2106 <97623925+Icebreaker2106@users.noreply.github.com>
2022-01-13 20:35:05 +0200

    Add files via upload

---
+++ Ex 1.py
@@ -0,0 +1,9 @@
+#Harjutus1
+
+name = input("Please enter Your name! \n")
+
+age = int(input("Please enter Your age! \n"))
+
+year = str((2022 - age) + 100)
+
+print(name + ", You will be 100 years old in the year " + year) 
