c74fec5bfc8ab8b0332d2657fb537756dad4352d
Icebreaker2106 <97623925+Icebreaker2106@users.noreply.github.com>
2022-01-15 17:38:36 +0200

    Add files via upload

---
+++ km_to_miles.py
@@ -0,0 +1,4 @@
+kms = input("How many kilometers did You cycle today?\n")
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"You cycled  {kms}  kilometers today, which equals to {miles} miles")
