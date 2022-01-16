a5f38b53e777ec15761ac87d6e7675c46c7231bb
Icebreaker2106 <97623925+Icebreaker2106@users.noreply.github.com>
2022-01-16 23:06:54 +0200

    Add files via upload

---
+++ f1_tracks.py
@@ -0,0 +1,55 @@
+tracks = input("Enter a track or country which length You would like to know.\n")
+
+#convert = input("Would You like to see results in kilometers or miles?[K/M]") idk this random
+	
+if tracks == "Monza":
+	print ("The length of Autodromo Nazionale di Monza is 5 kilometers and 793 meters")
+elif tracks == "Silverstone" or "Great Britain":
+	print ("The length of Silverstone Circuit is 5 kilometers and 891 meters")
+elif tracks == "Suzuka" or "Japan":
+	print ("The length of Suzuka International Racing Course is 5 kilometers and 807 meters")
+elif tracks == "Interlagos" or "Brazil":
+	print ("The length of Interlagos is 4 kilometers 309 meters")
+elif tracks == "Imola":
+	print ("The length of Autodromo Internazionale Enzo e Dino Ferrari is 4 kilometers 909 meters")
+elif tracks == "Barcelona" or "Spain":
+	print ("The length of Circuit de Barcelona-Catalunya is 4 kilometers 675 meters")
+elif tracks == "Monaco":
+	print ("The length of Circuit de Monaco is 4 kilometers 675 meters")
+elif tracks == "Yas Marina" or "Abu Dhabi":
+	print ("The length of Yas Marina Circuit is 5 kilometers 281 meters")
+elif tracks == "Spa" or "Belgium":
+	print ("The length of Circuit de Spa-Francorchamps is 7 kilometers and 4 meters")
+elif tracks == "Gilles Villeneuve" or "Canada":
+	print ("The length of Circuit Gilles Villeneuve is 4 kilometers and 361 meters")
+elif tracks == "Austin" or "Circuit of the Americas":	
+	print ("The length of Circuit of	the Americas is 5 kilometers and 513 meters")
+elif tracks == "France" or "Paul Ricard":
+	print ("The length of Circuit Paul Ricard is 5 kilometers and 842 meters")
+elif tracks == "Zandvoort" or "Netherlands":
+	print ("The length of Circuit Zandvoort is 4 kilometers and 259 meters")
+elif tracks == "Hungaroring" or "Hungary":
+	print ("The length of Hungaroring is 4 kilometers and 381 meters")
+elif tracks == "Jeddah" or "Saudi Arabia":
+	print ("The length of Jeddah Corniche Circuit is 6 kilometers and 174 meters")
+elif tracks == "Miami":
+	print ("The length of Miami International Autodrome is 5 kilometers and 410 meters")
+elif tracks == "Red Bull Ring" or "Austria":
+	print ("The length of Red Bull Ring is 4 kilometers and 318 meters")
+elif tracks == "Shanghai" or "China":
+	print ("The length of Shanghai International Circuit is 5 kilometers and 451 meters")
+elif tracks == "Sochi" or "Russia":
+	print ("The length of Sochi Autodrom is 5 kilometers and 848 meters")
+elif tracks == "Albert Park" or "Australia":
+	print ("The length of Albert Park Circuit is 5 kilometers and 303 meters")
+elif tracks == "Mexico":
+	print ("The length of Autódromo Hermanos Rodríguez is 4 kilometers and 304 meters")
+elif tracks == "Bahrain":
+	print ("The lenth of Bahrain International Circuit is 5 kilometers and 412 meters")
+elif tracks == "Baku":
+	print ("The length of Baku City Circuit is 6 kilometers and 3 meters")
+elif tracks == "Singapore" or "Marina Bay":
+	print ("The length of Marina Bay Street Circuit is 5 kilometers and 63 meters")
+
+
+
